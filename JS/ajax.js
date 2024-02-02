778fc50b9c0083ff8c1434cb9c3396322ca5b683
Dmytro Kolomatsky <dkolomatsky@gmail.com>
2024-02-02 07:51:31 +0100

    добавив варіант с fetch запитом

--- JS/ajax.js
+++ JS/ajax.js
@@ -1,5 +1,7 @@
 //https://jsonplaceholder.typicode.com/posts
 
+// AJAX запит: --------------------------------------------------------
+/*
 const posts = new XMLHttpRequest();
 posts.open("GET", "https://jsonplaceholder.typicode.com/posts");
 posts.send();
@@ -23,3 +25,33 @@ function showPosts(obj) {
   });
   document.querySelector(".container").append(...posts);
 }
+*/
+
+// Fetch запит: ------------------------------------------------------------
+/*
+const postS = fetch("https://jsonplaceholder.typicode.com/posts");
+postS
+  .then((postS) => {
+    return postS.json();
+  })
+  .then((postS) => {
+    showPostS(postS);
+  })
+  .catch((err) => {
+    console.error("Data not recieved");
+  });
+
+function showPostS(data) {
+  const posts = data.map((post) => {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.insertAdjacentHTML(
+      "afterbegin",
+      `<a href="/"><h2 class="title">Post #${post.id}:  <span class="header">${post.title}</span></h2></a>
+      <p class="postBody">${post.body}</p>`
+    );
+    return block;
+  });
+  document.querySelector(".container").append(...posts);
+}
+*/
